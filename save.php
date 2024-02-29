3de5781cc7f7c95b3153bfff6c751d9db0ad3cdb
Pratiksha Tabhane <103399336+pratikshatabhane@users.noreply.github.com>
2024-02-29 20:09:52 +0530

    Add files via upload

---
+++ save.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$server = "localhost";
+$username ="root";
+$password = "";
+$dbname ="loginpage";
+$con = mysqli_connect($server, $username, $password,$dbname);
+
+if (!$con)
+{
+    echo "not connected";
+
+}
+else
+{
+    echo "connect";
+
+}
+
+
+
+
+?>
